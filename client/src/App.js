f26f8a738bbc94a9e7ed1bd380e1e298b4f2bc4d
John Rooney <rooneyjohn@gmail.com>
2023-11-06 10:16:39 -0500

    pausing on this idea

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,14 @@ import "./App.css";
 import MyCalendar from "./components/MyCalendar";
 import KanbanBoard from "./components/KanbanBoard";
 import myImage from './images/reliable_design_logo2.jpg';
-const data = require('./data.json');
 
 function App() {
+
 
   return (
     <div className="App">
       <img className="mainLogo" src={myImage} alt="Reliable Design Logo"/>
+
       <Routes>
         <Route
           path="/"
